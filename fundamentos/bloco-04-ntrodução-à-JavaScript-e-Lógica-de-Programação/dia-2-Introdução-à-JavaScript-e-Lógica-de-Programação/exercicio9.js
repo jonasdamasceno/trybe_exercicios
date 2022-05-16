ef65dbbbcb2d1c84b091bee3b861f5630776a691
JonasDamasceno <jonasmichael2021@outlook.com>
2022-05-16 16:03:01 -0300

    exercicio9, verificar numero impar

---
+++ fundamentos/bloco-04-ntrodução-à-JavaScript-e-Lógica-de-Programação/dia-2-Introdução-à-JavaScript-e-Lógica-de-Programação/exercicio9.js
@@ -0,0 +1,13 @@
+//Escreva um programa que defina três números em constantes e retorne true se pelo menos uma das três for ímpar. Caso contrário, ele retorna false.
+//Bonus: use somente um if.
+
+const number1 = 6;
+const number2 = 7;
+const number3 = 8;
+
+if(number1 % 2 !== 0 || number2 % 2 !== 0 || number3 % 2 !== 0){
+    console.log(true);
+}
+else{ 
+    console.log(false);
+}
