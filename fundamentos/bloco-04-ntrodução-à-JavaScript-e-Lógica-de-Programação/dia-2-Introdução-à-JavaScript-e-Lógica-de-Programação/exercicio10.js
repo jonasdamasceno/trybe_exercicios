5ce4179ab4107f688c9e8bce0654e399f620f230
JonasDamasceno <jonasmichael2021@outlook.com>
2022-05-16 16:14:15 -0300

    exercicio 10, verificar lucro

---
+++ fundamentos/bloco-04-ntrodução-à-JavaScript-e-Lógica-de-Programação/dia-2-Introdução-à-JavaScript-e-Lógica-de-Programação/exercicio10.js
@@ -0,0 +1,14 @@
+//Escreva um programa que se inicie com dois valores em duas constantes diferentes: o custo de um produto e seu valor de venda. A partir dos valores, calcule quanto de lucro (valor de venda descontado o custo do produto) a empresa terá ao vender mil desses produtos.
+//Atente que, sobre o custo do produto, incide um imposto de 20%.
+//Seu programa também deve emitir uma mensagem de erro e encerrar caso algum dos seus valores de entrada seja menor que zero.
+
+const custo = 2;
+const venda = 4;
+
+if (custo < 0 || venda < 0){
+ console.log("Erro, valores invalidos");
+}
+else if(custo > 0 && venda > 0){
+  let lucro = ((venda - (custo * 0.2))*1000)
+    console.log(lucro); 
+}
