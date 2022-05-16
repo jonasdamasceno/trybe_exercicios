0d87b8e8f2c2728b5abec926a3db9bd92d505601
JonasDamasceno <jonasmichael2021@outlook.com>
2022-05-16 11:03:44 -0300

    exercicio 2 SOMA

--- fundamentos/bloco-04-ntrodução-à-JavaScript-e-Lógica-de-Programação/dia-2-javascript-array-e-loop-for./exerciciosArrayForLoop.js
+++ fundamentos/bloco-04-ntrodução-à-JavaScript-e-Lógica-de-Programação/dia-2-javascript-array-e-loop-for./exerciciosArrayForLoop.js
@@ -11,6 +11,8 @@ Utilizando o array criado no exercício anterior imprima o resultado da divisão
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for(let number of numbers)
-  console.log(number);
 
+  const soma = numbers.reduce(function(soma, index){
+    return soma + index;
+  });
+    console.log(soma);
