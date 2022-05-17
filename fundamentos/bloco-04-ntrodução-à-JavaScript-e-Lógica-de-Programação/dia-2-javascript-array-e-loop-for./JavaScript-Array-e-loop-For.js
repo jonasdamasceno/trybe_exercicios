fefe62b74b16467f9ee5fb33bb8dfaf92222b7f2
JonasDamasceno <jonasmichael2021@outlook.com>
2022-05-17 13:52:49 -0300

    exercicio 2

--- fundamentos/bloco-04-ntrodução-à-JavaScript-e-Lógica-de-Programação/dia-2-javascript-array-e-loop-for./JavaScript-Array-e-loop-For.js
+++ fundamentos/bloco-04-ntrodução-à-JavaScript-e-Lógica-de-Programação/dia-2-javascript-array-e-loop-for./JavaScript-Array-e-loop-For.js
@@ -10,6 +10,5 @@ Utilizando for, crie um array que vá de 1 até 25 e imprima o resultado;
 Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2.*/
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-  for(let index = 0; index < numbers.length; index += 1) {
-    console.log(numbers[index]);
-  }
+const total = numbers.reduce((total, currentElement) => total + currentElement);
+ console.log(total)
