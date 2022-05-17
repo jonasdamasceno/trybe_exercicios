0f1dcbfa7305ffc103454b865ca9cad5ae45dfff
JonasDamasceno <jonasmichael2021@outlook.com>
2022-05-17 13:31:33 -0300

    exercicio array

--- fundamentos/bloco-04-ntrodução-à-JavaScript-e-Lógica-de-Programação/dia-2-javascript-array-e-loop-for./exerciciosArrayForLoop.js
+++ fundamentos/bloco-04-ntrodução-à-JavaScript-e-Lógica-de-Programação/dia-2-javascript-array-e-loop-for./exerciciosArrayForLoop.js
@@ -1,19 +1,5 @@
-/*Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log();
-Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
-Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
-A média aritmética é o resultado da soma de todos os elementos divido pelo número total de elementos.
-Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
-🚀 Utilizando for, descubra qual o maior valor contido no array e imprima-o;
-Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
-Utilizando for, descubra qual o menor valor contido no array e imprima-o;
-Utilizando for, crie um array que vá de 1 até 25 e imprima o resultado;
-Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2.*/
-
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-  let total = 0
-  for (let index = 0; index < numbers.length; index++){
-    total += numbers[index]
-  }
-
-  
-    console.log(;
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu.indexOf("serviços");
+let indexOfPortfolio = menu.indexOf("Portfólio");
+menu.push("contato")
+console.log(menu);
