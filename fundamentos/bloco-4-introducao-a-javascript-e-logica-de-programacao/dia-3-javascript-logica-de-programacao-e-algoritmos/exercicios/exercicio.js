bfb10c49bb8aabbbac967da80dccdc9ffe52dda4
JonasDamasceno <jonasmichael2021@outlook.com>
2022-05-21 15:15:00 -0300

    exercicio 1, fatorial

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios/exercicio.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios/exercicio.js
@@ -1,8 +1,9 @@
-let word = 'banana';
-let palavraReversa = '';
+//O fatorial é representado pelo sinal !
+//! = 4 x 3 x 2 x 1 = 24
 
-for (let index = 0; index < word.length; index += 1) {
-  palavraReversa += word[word.length - 1 - index];
-}
-
-console.log(palavraReversa);
+  let Fatorial = 10;
+    let resultado = 1;
+    for(let index = Fatorial; index > 1; index -= 1){
+      resultado *= index;
+    }
+    console.log(resultado);
