4df567b0d55b92371037814134f04583ce9271db
JonasDamasceno <jonasmichael2021@outlook.com>
2022-05-21 18:29:19 -0300

    exercicio bonus 2, piramide de asteriscos

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios/exercicio.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios/exercicio.js
@@ -27,8 +27,8 @@ let n = 5;
 let aux = "";
 for (let linha = 1; linha <= n; linha += 1) {
   for (let coluna = 1; coluna <= n; coluna += 1) {
+    if(coluna === linha)
     aux += "*";
   }
   console.log(aux);
-  aux = "";
 }
