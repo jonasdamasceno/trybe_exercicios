e5845d9f5335c2b99cfdfebadde140156a900ae4
JonasDamasceno <jonasmichael2021@outlook.com>
2022-05-21 15:28:06 -0300

    exercicio 2, Anagrama

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios/exercicio.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios/exercicio.js
@@ -1,6 +1,5 @@
 //O fatorial é representado pelo sinal !
 //! = 4 x 3 x 2 x 1 = 24
-
   let Fatorial = 10;
     let resultado = 1;
     for(let index = Fatorial; index > 1; index -= 1){
@@ -8,8 +7,10 @@
     }
     console.log(resultado);
 
-    // Agora, desenvolva um algoritmo que é capaz de inverter uma palavra.
+// Agora, desenvolva um algoritmo que é capaz de inverter uma palavra.
      let palavra = "banana";
      let retornoAnagrama = palavra.split('').reverse('').join('');
       console.log(retornoAnagrama);
+
+//Escreva dois algoritmos: um que retorne a maior palavra deste array e outro que retorne a menor. Considere o número de caracteres de cada palavra.
     
