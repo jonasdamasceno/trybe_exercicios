6044dba1aa7949e14becc9bcd80abe70e59e2a99
JonasDamasceno <jonasmichael2021@outlook.com>
2022-05-21 12:43:14 -0300

    exercicio 9, divisão por dois

--- fundamentos/bloco-04-ntrodução-à-JavaScript-e-Lógica-de-Programação/dia-2-javascript-array-e-loop-for./JavaScript-Array-e-loop-For.js
+++ fundamentos/bloco-04-ntrodução-à-JavaScript-e-Lógica-de-Programação/dia-2-javascript-array-e-loop-for./JavaScript-Array-e-loop-For.js
@@ -9,10 +9,14 @@ Utilizando for, descubra qual o menor valor contido no array e imprima-o;
 Utilizando for, crie um array que vá de 1 até 25 e imprima o resultado;
 Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2.*/
 
-let numbers = [];
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
   for(let index = 1; index <= 25; index += 1){
     numbers.push(index);
   }
-    console.log(numbers);
+    if(numbers != 0){
+    for (let index = 0; index < numbers.length; index += 1) {
+      console.log(numbers[index] / 2);
+    } 
+    }; 
 
-//lembrar das diferenças entre os [] e os ()
+
