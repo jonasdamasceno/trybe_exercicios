1c1f90d494d665b96677bd56a5925ae1ae0ace8f
JonasDamasceno <jonasmichael2021@outlook.com>
2022-05-22 20:59:01 -0300

    para fixar 2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/forInForOf.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/forInForOf.js
@@ -1,3 +1,4 @@
+//Usando o objeto abaixo, utilize for/in e imprima no console a mensagem 'Olá xxxxx' para cada nome, substituindo o xxxxx pelo nome em questão.
 let names = {
     person1: 'João',
     person2: 'Maria',
@@ -5,4 +6,15 @@ let names = {
   };    
   for(let index in names){
   console.log("ola" + " " + names[index])
+  };
+
+  //Usando o objeto abaixo, utilize for/in e imprima um console.log com as chaves e valores desse objeto.
+  let car = {
+    model: 'A3 Sedan',
+    manufacturer: 'Audi',
+    year: 2020
+  };
+
+  for(let key in car){
+    console.log(key, car[key]);
   };
