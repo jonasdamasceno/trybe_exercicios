d3f6081b8ae1319b03a0d9ab73bf5616d3488601
JonasDamasceno <jonasmichael2021@outlook.com>
2022-05-22 17:53:18 -0300

    termino

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios/exercicio.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios/exercicio.js
@@ -25,10 +25,11 @@ array.split(' ').array.forEach(word => {
 
 let n = 5;
 let aux = "";
-for (let linha = 1; linha <= n; linha += 1) {
-  for (let coluna = 1; coluna <= n; coluna += 1) {
+for (let linha = 1; linha <= n; linha -= 1) {
+  for (let coluna = 1; coluna <= n; coluna -= 1) {
     if(coluna === linha)
     aux += "*";
   }
   console.log(aux);
 }
+//fim
