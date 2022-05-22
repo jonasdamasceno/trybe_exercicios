90eaf782c54aa7477f03c50a0003b8f9ce2a30df
JonasDamasceno <jonasmichael2021@outlook.com>
2022-05-22 19:37:40 -0300

    exercicio para fixar

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/objetos.js
@@ -0,0 +1,11 @@
+
+let competidor = {
+    nome: "Marta",
+    lastName: "silva",
+    age: 34,
+    medals: {
+        golde: 2,
+        silver: 3,
+    },
+}
+console.log("A jogadora" + " " + competidor["nome"] + " " + competidor["lastName"] + " " + "tem" + " " + competidor["age"] + " " +"de idade" );
