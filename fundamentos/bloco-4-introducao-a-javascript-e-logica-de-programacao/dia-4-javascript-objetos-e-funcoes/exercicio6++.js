30ccc862f5ade2f96eda2d183a0284b636189b4b
JonasDamasceno <jonasmichael2021@outlook.com>
2022-05-22 22:46:31 -0300

    exercicio 6, 7 e 8

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicio6++.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicio6++.js
@@ -14,4 +14,13 @@ let leitor = {
 
   for(let proprieties in leitor){
     console.log("o livro favorito de" + " " + leitor["nome"] + leitor["sobrenome"] + " " + 'se chama "' + " " + leitor["livrosFavoritos"][0]["titulo"] + '"' );
-  }
+  };
+  //- Adicione um novo livro favorito na chave livrosFavoritos, que é um array. Atribua a esta chave um objeto contendo as seguintes informações:
+  leitor.livrosFavoritos.push = {
+    titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+    autor: 'JK Rowling',
+    editora: 'Rocco',
+  },
+  //Acesse as chaves nome e livrosFavoritos e faça um console.log no seguinte formato: "Julia tem 2 livros favoritos".
+    console.log(leitor["nome"] + " " + leitor.livrosFavoritos.length + " " + "livros favoritos.");
+    console.log(leitor.livrosFavoritos)
