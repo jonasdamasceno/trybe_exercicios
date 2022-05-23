544572e1b0d1d4ed23c4716b28411dda41690e73
JonasDamasceno <jonasmichael2021@outlook.com>
2022-05-22 22:27:53 -0300

    exercicio 5, necessario rever

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/objetosEForIn.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/objetosEForIn.js
@@ -15,4 +15,19 @@ let info = {
 
   //Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves. Valor esperado no console:
 
-  
+  let info2 = {
+      personagem: "Tio Patinhas",
+      origem: "Cristmas on Bear Mountain, Dell's Four Color Comics #178",
+      nota: "O último MacPatinhas",
+      recorrente: "Sim", 
+  };
+  for(let key2 in info2){
+    if(key2 === "recorrente" &&
+    info[key2] === "sim" &&
+    info2[key2] === "sim"){
+      console.log("ambos recorrentes")
+    } else {
+  console.log(info[key] + "e" + info2[key2]);
+    }
+  console.log(Object.values(info) + " " + "e" + " " + Object.values(info2));
+  }
