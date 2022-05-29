697bb4ccf6c091088ccb8fca09fa9844eebca529
JonasDamasceno <jonasmichael2021@outlook.com>
2022-05-29 09:15:44 -0300

    exercicio 3

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -64,6 +64,28 @@ function buttonHoliday(button){
 };
 buttonHoliday('Feriado!!');
 
+//Implemente uma função que adicione ao botão "Feriados" um evento de "click" que muda a cor de fundo dos dias que possuem a classe "holiday".
+//É interessante que este botão possua também a lógica inversa. Ao ser clicado novamente ele retorna à configuração inicial com a cor "rgb(238,238,238)"
+
+function destacarFeriados() {
+  let clickBotaoFeriado = document.querySelector('#btn-holiday');
+  let diasDeFeriado = document.querySelectorAll('.holiday')
+  let backgroundColor = 'rgb(238,238,238)';
+  let cor = 'blue';
+
+  clickBotaoFeriado.addEventListener('click', function() {
+    for (let index = 0; index < diasDeFeriado.length; index += 1) {
+      if (diasDeFeriado[index].style.backgroundColor === cor) {
+        diasDeFeriado[index].style.backgroundColor = backgroundColor;
+      } else {
+        diasDeFeriado[index].style.backgroundColor = cor;
+      }
+    }
+  })
+};
+
+destacarFeriados();
+
 
 
  
