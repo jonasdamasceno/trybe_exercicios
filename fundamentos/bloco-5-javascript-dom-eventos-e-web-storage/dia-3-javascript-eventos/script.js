2b78aad35acd03fc00bb4af476dffb37fbf94531
JonasDamasceno <jonasmichael2021@outlook.com>
2022-05-29 11:44:33 -0300

    exercicio 7

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -149,3 +149,29 @@ function mouseOut(){
 };
 mouseOver();
 mouseOut();
+
+// Implemente uma função que adiciona uma tarefa personalizada ao calendário. A função deve receber como parâmetro a string com o nome da tarefa (ex: "cozinhar") e criar dinamicamente um elemento com a tag <span> contendo a tarefa.
+// O elemento criado deverá ser adicionado como filho/filha da tag <div> que possui a classe "my-tasks".
+
+function addAppointment(tasks){
+  let appointment = document.querySelector('.my-tasks');
+  let elementapponintment = document.createElement('span');
+
+  elementapponintment.innerHTML = tasks;
+  appointment.appendChild(elementapponintment);
+};
+addAppointment('limpar casa');
+
+// Implemente uma função que adiciona uma legenda com cor para a tarefa criada no exercício anterior. Esta função deverá receber como parâmetro uma string ("cor") e criar dinamicamente um elemento de tag <div> com a classe task.
+// O parâmetro cor deverá ser utilizado como cor de fundo da <div> criada.
+// O elemento criado deverá ser adicionado como filho/filha da tag <div> que possui a classe "my-tasks".
+
+function addCaption(color){
+  let tasksConteiner = document.querySelector('.my-tasks');
+  let elementTag = document.createElement('div');
+
+  elementTag.className = 'task';
+  elementTag.style.backgroundColor = color
+  tasksConteiner.appendChild(elementTag);   
+};
+addCaption('orange');
